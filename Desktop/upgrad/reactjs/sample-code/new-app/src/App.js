38b933dfd371d72f38a22fa5491074aeeac711c8
sidoncode <siddharth54330@gmail.com>
2022-05-19 21:08:26 +0530

    use axios

--- Desktop/upgrad/reactjs/sample-code/new-app/src/App.js
+++ Desktop/upgrad/reactjs/sample-code/new-app/src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
 import './App.css'
 
-import HookCounterOne from './components/HookCounterOne'
+import DataFetching from './components/DataFetching'
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <HookCounterOne/>
+      <DataFetching/>
       </div>
     )
   }
