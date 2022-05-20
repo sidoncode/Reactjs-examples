8d3b623b7874a498095b1b720491fff01b4b523b
sidoncode <siddharth54330@gmail.com>
2022-05-20 19:44:00 +0530

    user context

--- Desktop/upgrad/reactjs/sample-code/new-app/src/App.js
+++ Desktop/upgrad/reactjs/sample-code/new-app/src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react'
 import './App.css'
 
-import DataFetching from './components/DataFetching'
+import ComponentC from './components/ComponentC'
+
+export const UserContext = React.createContext()
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <DataFetching/>
+
+      <UserContext.Provider value={'Mr. Pavan'}>
+        <ComponentC/>
+      </UserContext.Provider>
+        
       </div>
     )
   }
